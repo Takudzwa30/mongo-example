961f79b001524a8317a49eb3baa8f0ed0db923e4
Takudzwa Allen Mushai <34072789+Takudzwa30@users.noreply.github.com>
2024-07-01 11:02:14 +0400

    added database

--- util/database.js
+++ util/database.js
@@ -1,8 +1,30 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb");
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const MongoClient = mongodb.MongoClient;
 
-module.exports = sequelize;
+let _db;
+
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://takudzwamushai:QtOFVsXgKrdHKl9c@cluster0.fivjgzv.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0"
+  )
+    .then((client) => {
+      console.log("Connected");
+      _db = client.db();
+      callback();
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No Database found!!!";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
